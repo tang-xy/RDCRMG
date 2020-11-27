b4b37af9e97bcfe2630ba6ad3eb3bb6b9d1791b6
tang-xy <a123654789654@yeah.net>
2020-11-27 17:59:33 +0800

    更新数据源

--- ClipByJson.py
+++ ClipByJson.py
@@ -19,5 +19,5 @@ if __name__ == "__main__":
             dic["features"] = [features[i]]
             json_feature_str = json.dumps(dic)
             json_file.write(json_feature_str)
-        clip_poly('temp/{0}.json', i, search_time)
+        clip_poly('temp/{0}.json'.format(i), str(i), search_time)
     shutil.rmtree('temp')
