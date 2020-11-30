13d5b4009f72486cf22f8f86814caa13a2ec6c36
tang-xy <a123654789654@yeah.net>
2020-11-30 14:12:10 +0800

    去黑边

--- ClipByPoly.py
+++ ClipByPoly.py
@@ -98,4 +98,4 @@ def clip_poly(jsonpath, task_id, search_time):
     grid_dic = None
     shutil.rmtree(temppath)
     end = time()
-    print("任务{1}耗时{0}，涉及{2}个格网".format(end-start, task_id, len(unitblock_list)))
+    print("任务{1}耗时{0}，涉及{2}个条带".format(end-start, task_id, len(unitblock_list)))
