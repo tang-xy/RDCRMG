d3d2f5798bc56e02f76864eea5bfd338a3b76a56
tang-xy <a123654789654@yeah.net>
2021-01-01 16:47:44 +0800

    debug

--- temp.py
+++ temp.py
@@ -13,8 +13,11 @@ for root, dirnames, filenames in os.walk(rootpath2):
 for root, dirnames, filenames in os.walk(rootpath1):
     for filename in filenames:
         path1 = os.path.join(root, filename)
+        if filename not in filedic.keys():
+            print(path1 + " not exist")
+            continue
         path2 = filedic[filename]
         if open(path1).read() != open(path2).read():
-            print(path1)
+            print(path1 + " error")
 
 print("well done")
